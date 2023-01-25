ddf4e8ee5bf66ef109ac69ec949e13996615113b
Raiyan Khalil Shuvo <50388868+RaiyanKhalil@users.noreply.github.com>
2023-01-24 21:16:40 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route exact path="/" element={<Layout />}>
           <Route index element={<Home />} />
           {/* <Route path="about" element={<About />} />
           <Route path="dashboard" element={<Dashboard />} /> */}
